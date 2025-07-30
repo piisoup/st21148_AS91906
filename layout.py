f4e12c343d982996216582d377e4a74932ff21d3
piisoup <104709813+piisoup@users.noreply.github.com>
2025-07-31 11:46:01 +1200

    Add files via upload

--- layout.py
+++ layout.py
@@ -1,10 +1,3 @@
-'''
-This is just going to be for working on the layout of my cat adoption program
-By Daisy Chamberlain
-
-started 18/07/2025
-'''
-
 from tkinter import *
 from tkinter import ttk
 
@@ -15,7 +8,9 @@ class CatAdoption:
     def __init__(self):
         self.root = Tk()
         self.root.title("Adopt A Cat!! :-)")
-        self.root.geometry("450x350")
+        self.root.geometry("700x720")
+
+        self.root.resizable(False, False)
 
         # Container for all frames
         self.container = Frame(self.root)
@@ -26,8 +21,7 @@ class CatAdoption:
         # Dictionary to hold the frames
         self.frames = {}
 
-        # Add each frame to the dictionary
-        for F in (MainFrame, AdoptionFrame):
+        for F in (MainFrame,):
             frame = F(self.container, self)
             self.frames[F.__name__] = frame
             frame.grid(row=0, column=0, sticky="nsew")
@@ -35,19 +29,8 @@ class CatAdoption:
         # Show initial frame
         self.show_frame("MainFrame")
 
-
-
         self.bottom_frame = Frame(self.root, bg="#D6CCC2")
         self.bottom_frame.pack(side="bottom", fill="x")
-
-        # quit button
-        button_quit = Button(self.bottom_frame, text="Exit", 
-                                                            font=("PMingLiU", 14, "bold"),
-                                                            fg="#827268",
-                                                            width=15,
-                                                            height=1,
-                                                            command=self.quit_app)
-        button_quit.pack(padx=40, fill='x', pady=(0, 10))
 
     def show_frame(self, frame_name):
         # shows requested frame
@@ -84,7 +67,6 @@ class MainFrame(Frame):
         label.pack(pady=20, fill='x', padx=10)
 
         # Button to go to Adoption page
-        # This button doesnt work yet, WIP
         to_adopt_button = Button(self, text="Go", bg="#EDEDE9",
                                  font=("PMingLiU", 16, "bold"),
                                  fg="#827268",
@@ -92,6 +74,15 @@ class MainFrame(Frame):
                                  height=2,
                                  command=lambda: controller.show_frame("AdoptionFrame"))
         to_adopt_button.pack(pady=(85, 10), fill='x', padx=40)
+
+        # moved quit button to MainFrame as it's part of the homepage layout
+        button_quit = Button(self, text="Exit",
+                                 font=("PMingLiU", 14, "bold"),
+                                 fg="#827268",
+                                 width=15,
+                                 height=1,
+                                 command=controller.quit_app)
+        button_quit.pack(padx=40, fill='x', pady=(0, 10))
 
 # run the application
 if __name__ == "__main__":
